452c49b9a268d367987e997328ab9193be13d785
William <william@inapulse.com>
2024-03-18 19:39:03 -0400

    home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,14 @@ const config: Config = {
       }
 
     },
+    screens: {
+      'home-lg': '1268px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 };
