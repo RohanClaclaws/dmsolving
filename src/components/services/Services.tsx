0e457aa57aeb27904fc5e3d34aa078b5b277833a
rohan <rohan@claclaws.com>
2024-03-19 12:12:39 -0400

    vercel deploy

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -249,7 +249,7 @@ const Services = () => {
 
           <div className="text-center py-[85px]">
             <h3 className=" font-syne text-[21px] mb-2 sm:text-[27px] text-[#ffffff]">Tell about your project !</h3>
-            <h1 className="font-syne font-[600] text-[30px] sm:text-[40px] md:text-[50px] lg:text-[60px] xl:text-[70px] text-[#fff]">Let's Chat With <span className="text-[#00EbFF] ">DMSolving </span></h1>
+            <h1 className="font-syne font-[600] text-[30px] sm:text-[40px] md:text-[50px] lg:text-[60px] xl:text-[70px] text-[#fff]">Let&apos;s Chat With <span className="text-[#00EbFF] ">DMSolving </span></h1>
           </div>
 
 
