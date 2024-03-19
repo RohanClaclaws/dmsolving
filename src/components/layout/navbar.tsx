ac4f788f8df160556cb4e8682b23cf08b6248ed7
Frank Jia <jurong@claclaws.com>
2024-03-19 09:04:44 -0400

    nav-bar-v1.0.1

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return(
+        <header>
+            <div className="logoArea">
+                left
+            </div>
+            <div className="nav-menu">
+                menu
+            </div>
+            <div className="contact">
+                contact
+            </div>            
+        </header>
+    )
+}
+
+export default Navbar
