db9066c83b944f6db29140580dca6aec33b5387e
Frank Jia <jurong@claclaws.com>
2024-03-21 19:09:18 -0400

    navbar-v2.0.3

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react"
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false)
+    const [triggerSubmenu, setTriggerSubmenu] = useState(false)
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0) {
@@ -37,6 +38,21 @@ const Navbar = () => {
         {
             label: "Services",
             href: "/services",
+            subMenu: [
+                {
+                    label: "Web Design",
+                    href: "/webDesign"
+                },
+                {
+                    label: "Digital Marketing",
+                    href: "/digitalMarketing"
+                },
+                {
+                    label: "App Development",
+                    href: "/appDevelopment"
+                }
+
+            ]
         },
         {
             label: "Portfolio",
@@ -54,23 +70,52 @@ const Navbar = () => {
             label: "Request a Proposal",
             href: "/contact-us",
         },
-    ].map(({ label, href }) => (
+    ].map(({ label, href, subMenu }) => (
         <li
             key={href}
             className={`${styles.link} ${href === pathname && styles.active}`}
+            onMouseEnter={() => { setTriggerSubmenu(true) }}
+            onMouseLeave={() => { setTriggerSubmenu(false) }}
         >
             <Link
                 href={href}
+                className={styles.innerLink}
             >
                 {label}
+                {
+                    subMenu && <Image 
+                        className={styles.icon}
+                        src={"/images/navbar/arrow-down.png"} 
+                        alt={"arrow"} 
+                        height={33} 
+                        width={65}
+                        style={{transform: triggerSubmenu? "rotateX(0deg)" : "rotateX(-180deg)"}}
+                    />
+                }
             </Link>
+            {
+                subMenu && <ul className={styles.dropdownMenu}
+                    style={{transform: triggerSubmenu? "scaleY(1)" : "scaleY(0)"}}
+                >
+                    {
+                        subMenu.map(({label, href}) => (
+                            <li key={href}>
+                                <Link href={href}>                                    
+                                    {label}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            }
+
         </li>
     ))
     return (
         <header className={styles.headerArea}
             style={{
-                backgroundColor: isScrolled? `black`:`transparent`,
-                position: isScrolled? `fixed` : `absolute`,
+                backgroundColor: isScrolled ? `black` : `transparent`,
+                position: isScrolled ? `fixed` : `absolute`,
             }}
         >
             <div className="layout">
