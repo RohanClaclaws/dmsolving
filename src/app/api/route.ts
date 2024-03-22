a6dc49d8a586e243b0886338b7273c598090dc1e
Frank Jia <jurong@claclaws.com>
2024-03-22 14:06:36 -0400

    navbar-v2.0.4

---
+++ src/app/api/route.ts
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer")
+
+export async function POST(req: Request) {
+    const {name, email, subject, message } = await req.json()
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.elasticemail.com',
+        port: 2525,
+        secure: false,
+        auth: {
+          user: 'jurong@claclaws.com',
+          pass: 'B48D25C12E2F64B220FD2973B32A0BC7D98E'
+        }
+      });
+  
+      try {
+        await transporter.sendMail({
+          from: 'jurong@claclaws.com',
+          to: email,
+          subject: subject,
+          text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+        });
+  
+        return Response.json({ success: true });
+      } catch (error) {
+        console.error('Error sending email:', error);
+        return Response.json({ success: false, error: 'Failed to send email' });
+      }
+}
