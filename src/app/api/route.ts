61e79066c711e3814d394bd884d04f0a633d0b76
RohanClaclaws <139475519+RohanClaclaws@users.noreply.github.com>
2024-03-27 16:58:55 -0400

    Update route.ts
    Testing the email sender

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -7,8 +7,8 @@ export async function POST(req: Request) {
 
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true,
+        // port: process.env.SMTP_PORT,
+        // secure: true,
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASS
