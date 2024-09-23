54d15bef391145d543d3a5d25388112ff77f487b
unknown <lu@claclaws.com>
2024-09-23 10:58:32 -0400

    low case

---
+++ src/app/services/ai/page.tsx
@@ -0,0 +1,18 @@
+
+import Hero from "@/components/ai/Hero";
+import SecondSection from "@/components/ai/SecondSection";
+import ThirdSection from "@/components/ai/ThirdSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'AI | Dmsolving',
+  description: 'AI at Dmsolving'
+}
+
+export default function Page() {
+  return <>
+      <Hero/>
+      <SecondSection/>
+      <ThirdSection/>
+  </>
+}
